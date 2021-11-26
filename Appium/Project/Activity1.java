61a6a611292e7e5b0db5fea7cc3929615d4759ce
Shruti Joshi3 <shruti.joshi3@ibm.com>
2021-11-26 10:30:03 +0530

    Added appium files

---
+++ Appium/Project/Activity1.java
@@ -0,0 +1,58 @@
+package Appium_Project;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+
+public class Activity1 {
+	AndroidDriver<MobileElement> driver;
+	
+	
+	 @BeforeClass
+	  public void beforeClass() throws MalformedURLException {
+		 DesiredCapabilities Caps = new DesiredCapabilities();
+		 Caps.setCapability("deviceName", "Pixel4_TestDevice");
+		 Caps.setCapability("platformName", "android");
+		 Caps.setCapability("automationName", "UiAutomator2");
+		 Caps.setCapability("appPackage", "com.android.calculator2");
+		 Caps.setCapability("appActivity", ".Calculator");
+		 Caps.setCapability("noReset", true);
+		 
+		 //Set Appium URL
+		 URL RemotUrl = new URL("http://localhost:4723/wd/hub");
+		 driver = new AndroidDriver<MobileElement>(RemotUrl,Caps);
+	  }
+
+  @Test
+  public void f() {
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+	  
+	  driver.quit();
+  }
+
+}
